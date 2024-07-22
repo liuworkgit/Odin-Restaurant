e3741db18fd637145ccf1b8f251aa945937fd76d
moses21 <moses21@student.ubc.ca>
2024-07-22 13:58:03 -0700

    Finished implementing index.js.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,43 @@ import makeContact from './page-scripts/contact-page';
 import makeHomePg from './page-scripts/home-page';
 import makeMenuPg from './page-scripts/menu-page';
 
+document.getElementById('home-btn').addEventListener(
+    'click',
+    function () {
+        clearDiv();
+        makeHomePg();
+    },
+    false
+);
+document.getElementById('menu-btn').addEventListener(
+    'click',
+    function () {
+        clearDiv();
+        makeMenuPg();
+    },
+    false
+);
+document.getElementById('contact-btn').addEventListener(
+    'click',
+    function () {
+        clearDiv();
+        makeContact();
+    },
+    false
+);
+
+makeHomePg();
+console.log('Build successful.');
+
+// **********************************************
+// FUNCTIONS
+
 /**
  * Clears the contents of the content div.
  */
 function clearDiv() {
-    let div = document.getElementById("content");
+    let div = document.getElementById('content');
     while (div.firstChild) {
         div.removeChild(div.firstChild);
     };
 };
-
-console.log('Build successful.');
