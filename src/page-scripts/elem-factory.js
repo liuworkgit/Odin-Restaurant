15b336ca82679e2decbc1106524dca9dd375637d
moses21 <moses21@student.ubc.ca>
2024-07-22 10:57:54 -0700

    Implemented home page creation module in full, minor grammar adjustments to elem-factory documentation.

--- src/page-scripts/elem-factory.js
+++ src/page-scripts/elem-factory.js
@@ -1,7 +1,7 @@
 /**
- * Creates elements of the given type and with the given content.
- * @param {String} type - the element's type.
- * @param {String} content - the content of the element.
+ * Creates elements with the given type and content.
+ * @param {String} type - The element's type.
+ * @param {String} content - The element's content.
  * @returns HTMLElement
  */
 export default function makeElement(type, content) {
