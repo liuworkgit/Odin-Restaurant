32b23a37f36616a88bb0bc7855d298b1bd6717c6
moses21 <moses21@student.ubc.ca>
2024-07-22 13:50:15 -0700

    Implemented clearDiv function in index.js.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import makeElement from "./home-page";
+import makeContact from './page-scripts/contact-page';
+import makeHomePg from './page-scripts/home-page';
+import makeMenuPg from './page-scripts/menu-page';
 
-document.getElementById("content").append(
-    makeElement("h1", "Odin Restaurant"),
-    makeElement("p", "Delicious knowledge-filled dishes at the click of a button."),
-    makeElement("h2", "High quality and ready-to-eat."),
-    makeElement("p", "We at Odin Restaurant know your diet matters, which is why we make our dishes with only the most informative resources and ingredients.")
-);
+/**
+ * Clears the contents of the content div.
+ */
+function clearDiv() {
+    let div = document.getElementById("content");
+    while (div.firstChild) {
+        div.removeChild(div.firstChild);
+    };
+};
 
-console.log("Build successful.");
+console.log('Build successful.');
