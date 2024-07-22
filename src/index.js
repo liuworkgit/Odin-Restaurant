ba44eeb62c44cc06d4401481113dfc399073b3d3
moses21 <moses21@student.ubc.ca>
2024-07-22 10:27:52 -0700

    Fixed issue where document.getElementById('content').append wasn't working.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import makeElement from "./home-page";
 
-const homeElements = [
+document.getElementById("content").append(
     makeElement("h1", "Odin Restaurant"),
     makeElement("p", "Delicious knowledge-filled dishes at the click of a button."),
     makeElement("h2", "High quality and ready-to-eat."),
     makeElement("p", "We at Odin Restaurant know your diet matters, which is why we make our dishes with only the most informative resources and ingredients.")
-];
-document.getElementById("content").append(homeElements);
+);
 
 console.log("Build successful.");
