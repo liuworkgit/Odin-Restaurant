3147942e17a9897ca32175c30c5ca078de7d56f1
moses21 <moses21@student.ubc.ca>
2024-07-22 13:42:41 -0700

    Finished implementation of menu page module.

--- src/page-scripts/menu-page.js
+++ src/page-scripts/menu-page.js
@@ -1,12 +1,28 @@
-/**
- * <h1>Menu</h1>
-        
-        <h2>Plugins and Loaders Spaghetti</h2>
-        <img src="src/assets/webpack-icon.jpg" alt="Webpack logo" height="300px" width="300px">
-        <p>An excellent way to enhance the tastiness of your project with different dependencies! Allows you to modify how you make your project.</p>
+import makeElement from './elem-factory';
 
-        <h2>HTML Webpack Plugin Pudding</h2>
-        <img src="src/assets/webpack-icon.jpg" alt="Webpack logo" height="300px" width="300px">
-        <p>Want to dynamically cook up an HTML file? This is how! Just toss in a template and let the plugin do the work for you.</p>
- * 
+/**
+ * Creates the content of the menu page.
  */
+export default function makeMenuPg() {
+       document.getElementById('content').append(
+              makeElement('h1', 'Menu'),
+              makeElement('h2', 'Plugins and Loaders Spaghetti'),
+              makeMenuImg(),
+              makeElement('p', 'An excellent way to enhance the tastiness of your project with different dependencies! Allows you to modify how you make your project.'),
+              makeElement('h2', 'HTML Webpack Plugin Pudding'),
+              makeMenuImg(),
+              makeElement('p', 'Want to dynamically cook up an HTML file? This is how! Just toss in a template and let the plugin do the work for you.')
+       )
+};
+
+/**
+ * Creates the image elements of the menu page.
+ */
+function makeMenuImg() {
+       let img = document.createElement('img');
+       img.setAttribute('src', '/Users/sen/Desktop/COMPUTER STUFF/webdev/odin-restaurant/src/assets/webpack-icon.jpg');
+       img.setAttribute('alt', 'Webpack logo');
+       img.setAttribute('height', '300px');
+       img.setAttribute('width', '300px');
+       return img;
+};
